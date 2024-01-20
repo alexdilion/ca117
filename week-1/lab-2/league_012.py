38a6ee78a9cc52c690c070bd5ee89f006eca02d8
alexdilion <alexdilion@protonmail.com>
2024-01-20 22:00:21 +0000

    beep boop

--- week-1/lab-2/league_012.py
+++ week-1/lab-2/league_012.py
@@ -2,33 +2,31 @@
 
 import sys
 
-headings = ["POS", "CLUB", "P", "W", "D", "L", "GF", "GA", "GD", "PTS"]
-maxClubLength = 0
-
 def processLine(line):
     data = []
     tokens = line.split()
-    
+
     data.append(tokens[0])
     data.append(" ".join(tokens[1:-8]))
-    
+
     for token in tokens[-8:]:
         data.append(token)
-    
+
     return data
 
-rows = [headings]
+maxClubLength = 0
+rows = [["POS", "CLUB", "P", "W", "D", "L", "GF", "GA", "GD", "PTS"]]
 for line in sys.stdin:
     club = processLine(line.strip())
     rows.append(club)
-    
+
     if len(club[1]) > maxClubLength:
         maxClubLength = len(club[1])
 
 for row in rows:
     rowString = f"{row[0]:>3s} {row[1]:{maxClubLength}} {row[2]:>2s}"
-    
+
     for entry in row[-7:]:
         rowString += f" {entry:>3s}"
-    
+
     print(rowString)
