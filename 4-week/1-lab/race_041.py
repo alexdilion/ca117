a8f2baee559688e36e9a2acf99327ec08ab0e065
alexdilion <alexdilion@protonmail.com>
2024-02-08 14:55:49 +0000

    Small changes

--- 4-week/1-lab/race_041.py
+++ 4-week/1-lab/race_041.py
@@ -11,12 +11,15 @@ def convertTime(time):
         return 0
 
 runners = {}
+bestRunner = ""
 for runner in sys.stdin:
     name, *timeData = runner.split()
     bestTime = min([convertTime(time) for time in timeData])
     
-    if bestTime != 0:
+    if bestTime:
         runners[name] = f"{bestTime // 60}:{bestTime % 60:02}"
 
-bestRunner = min(runners, key=lambda k: convertTime(runners[k]))
+        if not bestRunner or bestTime < convertTime(runners[bestRunner]):
+            bestRunner = name
+
 print(f"{bestRunner} : {runners[bestRunner]}")
