6a90f8c0e48b66686fa0cf0d1dba63d32f03dc86
alexdilion <alexdilion@protonmail.com>
2024-02-13 17:38:54 +0000

    Small changes

--- 5-week/1-lab/nightclub_051.py
+++ 5-week/1-lab/nightclub_051.py
@@ -4,8 +4,7 @@ import sys
 
 maxCapacity = int(sys.stdin.readline())
 totalPeople = 0
-deniedEntry = 0
-
+entriesDenied = 0
 for group in sys.stdin:
     tokens = group.strip().split()
     isLeaving, size = tokens[0] == "leave", int(tokens[1])
@@ -15,6 +14,6 @@ for group in sys.stdin:
     elif totalPeople + size <= maxCapacity:
         totalPeople += size
     else:
-        deniedEntry += 1
+        entriesDenied += 1
 
-print(deniedEntry)
+print(entriesDenied)
--- 5-week/1-lab/speed_051.py
+++ 5-week/1-lab/speed_051.py
@@ -4,7 +4,6 @@ import sys
 
 speeds = []
 prevTime, prevDistance = 0, 0
-
 for line in sys.stdin.readlines()[1:]:
     newTime, newDistance = [int(v) for v in line.split()]
     speeds.append((newDistance - prevDistance) / (newTime - prevTime))
