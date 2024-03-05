0feb323bf3d1140abc28cd2e5d78eadf30f04634
alexdilion <alexdilion@protonmail.com>
2024-03-05 15:28:54 +0000

    Add week 8 lab 1 solutions

---
+++ 8-week/1-lab/bank_081.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+
+class BankAccount:
+    def set_attributes(self, name, number, balance):
+        self.name = name
+        self.number = number
+        self.balance = balance
+        
+    def print_attributes(self):
+        print(f"Name: {self.name}")
+        print(f"Account number: {self.number}")
+        print(f"Balance: {self.balance:.02f}")
+    
+    def deposit(self, amount):
+        self.balance += amount
---
+++ 8-week/1-lab/student_081.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+
+class Student:
+    def set_attributes(self, sid, name, modlist):
+        self.sid = sid
+        self.name = name
+        self.modlist = modlist
+    
+    def print_attributes(self):
+        print(f"ID: {self.sid}")
+        print(f"Name: {self.name}")
+        print(f"Modules: {', '.join(self.modlist)}")
+    
+    def add_module(self, module):
+        if module not in self.modlist:
+            self.modlist.append(module)
+    
+    def del_module(self, module):
+        self.modlist.remove(module)
